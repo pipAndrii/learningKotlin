1acfeb571fbe6197ae25ee533273eac745fcc2a2
Vasyl <vasyl.purskyi.zy@renesas.com>
2023-08-08 22:48:05 +0300

    firstGuessNum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.math.abs
+import kotlin.math.pow
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    guessMyNum()
 }
+fun guessMyNum()
+{
+    var pcNum = 50
+    println("Hello guess some num from 0 to 100 and I will do magic")
+    var input:String = ""
+    while (input != "S") {
+        pcNum = guess(pcNum,input)
+        println("your num is bigger than $pcNum")
+        println("Enter command\nY - yes\nN - no\nS - stop")
+        input = readln()
+    }
+    println("Thanks for your time")
+}
+fun guess(temp:Int, input:String):Int
+{
+    var pcNum = temp
+
+    if(input == "Y") {
+        pcNum +=pcNum/2
+    }
+    if(input == "N") {
+        pcNum -= pcNum / 2
+    }
+
+    return pcNum
+}
+
