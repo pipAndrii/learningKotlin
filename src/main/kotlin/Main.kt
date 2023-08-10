7664339b1a4d94facdc333ef480438d8c5a49257
Vasyl <vasyl.purskyi.zy@renesas.com>
2023-08-10 11:58:11 +0300

    anonymus

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,17 +9,10 @@ fun main() {
 
 fun calculator(key: Char): (Double, Double) -> Double {
     return when (key) {
-        '+' -> ::sum
-        '-' -> ::sub
-        '*' -> ::mul
-        '/' -> ::div
-        else -> ::empty
+        '+' -> fun (a: Double, b: Double) = a + b
+        '-' -> fun (a: Double, b: Double) = a - b
+        '*' -> fun (a: Double, b: Double) = a * b
+        '/' -> fun (a: Double, b: Double) = a / b
+        else -> fun(a: Double, b: Double) = 0.0
     }
 }
-
-fun sum(a: Double, b: Double) = a + b
-fun sub(a: Double, b: Double) = a - b
-fun mul(a: Double, b: Double) = a * b
-fun div(a: Double, b: Double) = a / b
-
-fun empty(a: Double, b: Double) = 0.0
